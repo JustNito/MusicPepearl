420d740403535e6af248b1c3dca156823f292543
Артур Шакура <shakura2marta@gmail.com>
2020-07-11 03:22:27 +0300

    UUUUU

---
+++ index.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const config = require(`./config.json`);
+
+//function emoji (id) {
+//   return client.emojis.get(id).toString();
+//}
+
+bot.on('message', async message => {
+    let prefix = config.prefix;
+
+    if (!message.guild) return;
+
+    if (message.content == (prefix + "join")) {
+        if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.join();
+            //connection.play('Surprise - MazaFAKA.mp3');
+            const broadcast = bot.voice.createBroadcast();
+            broadcast.play('Surprise - MazaFAKA.mp3');
+            connection.play(broadcast);
+        } else {
+            message.reply('You need to join a voice channel first!');
+        }
+    }
+
+    if (message.content == (prefix + "leave")) {
+        if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.leave();
+        } else {
+            message.reply('You need to join a voice channel first!');
+        }
+    }
+
+    if (message.content.startsWith(prefix + "say")) {
+        message.delete();
+        message.channel.send(message.content.substr(5,));
+        return;
+    }
+
+    if (message.content == (prefix + "gif")) {
+        number = 4;
+        gifNumber = Math.floor(Math.random() * (number - 1 + 1)) + 1;
+        message.channel.send({ files: ["./gif/" + gifNumber + ".gif"] })
+        return;
+    }
+
+    if (message.content == "rainbow" && message.author.id == '249514842703003648') {
+        message.delete();
+        message.channel.send('<a:Rainbow:552553091686334498>');
+        return;
+    }
+});
+
+bot.login(config.token);
+bot.on('ready', () => {
+    console.log(bot.user.username + ' online');
+    bot.user.setPresence({ status: 'online', game: { name: 'с твоей мамашей', type: 0 } });
+});
