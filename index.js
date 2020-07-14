ea87f745c560db81fb82f8d4e02879c7a6598ba7
Артур Шакура <shakura2marta@gmail.com>
2020-07-15 01:05:31 +0300

    vksuc

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const {
     access_token,
 } = require('./config.json');
 const ytdl = require('ytdl-core');
-var https = require('https');
+const https = require('https');
+const ytsr = require('ytsr');
 const ffmpeg = require('ffmpeg');
 
 const client = new Discord.Client();
@@ -40,134 +41,180 @@ client.on('message', async message => {
         return
     }
     else if (message.content.startsWith(prefix + "play")) {
-        if (message.member.voice.channel) {
-            /*
-            const connection = await message.member.voice.channel.join();
-            const ytdl = require('ytdl-core');
-            connection.play(ytdl(message.content.substr(6,), { filter: 'audioonly' }));
-
-            const broadcast = client.voice.createBroadcast();
-            broadcast.play('/home/hydrabolt/audio.mp3');
-            connection.play(broadcast);
-            */
-            const connection = await message.member.voice.channel.join();
-            const broadcast = client.voice.createBroadcast();
-            broadcast.play(ytdl(message.content.substr(6,), { filter: 'audioonly' }));
-            connection.play(broadcast);
-        } else {
-            message.reply('You need to join a voice channel first!');
+        const voiceChannel = message.member.voice.channel;
+        if (!voiceChannel)
+            return message.reply("You need to be in a voice channel to play music!");
+        const arg = message.content.substr(6,).trim();
+        if (arg.startsWith("https://youtu.be/")) {
+            arg.replace("https://youtu.be/", "https://www.youtube.com/watch?v");
+            const connection = await voiceChannel.join();
+            const songInfo = await ytdl.getInfo(arg);
+            connection.play(ytdl(arg));
+            return message.channel.send("Play: " + songInfo.title);
         }
-        return
+        else if (arg.startsWith("https://www.youtube.com/watch?v")) {
+            const connection = await voiceChannel.join();
+            const songInfo = await ytdl.getInfo(arg);
+            connection.play(ytdl(arg));
+            return message.channel.send("Play: " + songInfo.title);
+        }
+        else {
+            message.reply("Invalid YouTube URL");
+            return
+        }
     }
     else if (message.content.startsWith(prefix + "vk")) {
-
-        const options = new URL('https://api.vk.com/method/users.get?user_ids=aff3ct&fields=status&access_token=' + access_token + '&v=5.120');
-
-        const req = https.request(options, (res) => {
-            res.setEncoding('utf8');
-            res.on('data', (chunk) => {
-                //console.log(chunk);
-                let obj = JSON.parse(chunk);
-                if (obj.error != undefined) {
-                    console.log(obj.error);
-                    return;
-                }
-                console.log(obj["response"][0].status_audio);
-                if (obj["response"][0].status_audio != undefined) {
-                    const ytsr = require('ytsr');
-                    let filter;
-                    let music = obj["response"][0].status_audio.artist + " - " + obj["response"][0].status_audio.title
-                    message.channel.send(music);
-                    ytsr.getFilters(music, function (err, filters) {
-                        if (err) {
-                            console.log("htp1");
-                            throw err;
-                        }
-                        filter = filters.get('Type').find(o => o.name === 'Video');
-                        ytsr.getFilters(filter.ref, function (err, filters) {
-                            if (err) {
-                                console.log("htp2");
-                                throw err;
-                            }
-                            var options = {
-                                limit: 1,
-                                nextpageRef: filter.ref,
-                            }
-                            ytsr(null, options, function (err, searchResults) {
-                                if (err) {
-                                    console.log("htp3");
-                                    throw err;
-                                }
-                                //const connection = await message.member.voice.channel.join();
-                                //const broadcast = client.voice.createBroadcast();
-                                //broadcast.play(ytdl(searchResults.items[0].link, { filter: 'audioonly' }));
-                                //connection.play(broadcast);
-                                console.log("результат мазафака");
-                                console.log(searchResults.items[0].link);
-                                vkplay(message, searchResults.items[0].link);
-                            });
-                        });
-                    });
-                }
-                else {
-                    message.channel.send("Добавьте музыку в статус");
-                }
-            });
-            res.on('end', () => {
-                console.log('No more data in response.');
-            });
-        });
-
-        req.on('error', (e) => {
-            console.error(`problem with request: ${e.message}`);
-        });
-
-        req.end();
-
-        return;
+        if (message.member.voice.channel) {
+            let arg = message.content.substr(4,).trim();
+            if (arg.startsWith("https://vk.com/")) {
+                arg = arg.substr(15,);
+                const options = new URL('https://api.vk.com/method/users.get?user_ids=' + arg + '&fields=status&access_token=' + access_token + '&v=5.120');
+                vkhttpsreq(options, message, vkmusicsearch);
+                return;
+            }
+            else {
+                message.reply("Invalid VK URL");
+                return
+            }
+        }
+        else {
+            message.reply('You need to join a voice channel first!');
+            return;
+        }
     }
-
-    else if (message.content.startsWith(prefix + "skip")) {
+    else if (message.content.startsWith(prefix + "stop")) {
         if (message.member.voice.channel) {
             const connection = await message.member.voice.channel.join();
-            const broadcast = client.voice.createBroadcast();
-            broadcast.end()
-            connection.play(broadcast);
+            connection.dispatcher.end();
+            return;
         } else {
-            message.reply('You need to join a voice channel first!');
+            return message.reply('You need to join a voice channel first!');
         }
-        return;
     }
     else if (message.content.startsWith(prefix + "leave")) {
         if (message.member.voice.channel) {
             const connection = await message.member.voice.channel.leave();
+            return;
         } else {
-            message.reply('You need to join a voice channel first!');
+            return message.reply('You need to join a voice channel first!');
         }
-        return;
     }
     else if (message.content.startsWith(prefix + "info")) {
         return;
     }
     else if (message.content.startsWith(prefix + "say")) {
         message.delete();
-        message.channel.send(message.content.substr(5,));
-        return;
+        return message.channel.send(message.content.substr(5,));
     }
     else if (message.content.startsWith(prefix + "gif")) {
         number = 4;
         gifNumber = Math.floor(Math.random() * (number - 1 + 1)) + 1;
-        message.channel.send({ files: ["./gif/" + gifNumber + ".gif"] })
-        return;
+        return message.channel.send({ files: ["./gif/" + gifNumber + ".gif"] });
     }
 });
 
 async function vkplay(message, result) {
     const connection = await message.member.voice.channel.join();
-    const broadcast = client.voice.createBroadcast();
-    broadcast.play(ytdl(result, { filter: 'audioonly' }));
-    connection.play(broadcast);
-    return;
+    const songInfo = await ytdl.getInfo(result);
+    connection.play(ytdl(result));
+    message.channel.send("Play: " + songInfo.title);
 }
+
+function vkhttpsreq(options, message, cb, nowplaymusic = undefined) {
+    const req = https.request(options, (res) => {
+        let obj = "";
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            //console.log(chunk);
+            obj += chunk;
+        });
+        res.on('end', () => {
+            //console.log('No more data in response.');
+            var o = JSON.parse(obj);
+            if (o.error != undefined) {
+                console.log(o.error);
+            }
+            else if (o["response"][0].status_audio != undefined) {
+                let music = o["response"][0].status_audio.artist + " - " + o["response"][0].status_audio.title
+                if (nowplaymusic == undefined) {
+                    cb(message, music)
+                    vkhttpsreq(options, message, cb, music)
+                }
+                else if (music == nowplaymusic) {
+                    vkhttpsreq(options, message, cb, music)
+                }
+                else {
+                    cb(message, music)
+                    vkhttpsreq(options, message, cb, music)
+                }
+            }
+            else {
+                message.channel.send("No music in status");
+            }
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
+    });
+
+    req.end();
+}
+
+function vkmusicsearch(message, music) {
+    let filter;
+    console.log(music);
+    ytsr.getFilters(music, function (err, filters) {
+        if (err) {
+            console.log("htp1");
+            try {
+                throw err;
+            }
+            catch (err) {
+                console.log(err)
+                vkmusicsearch(message, music);
+            }
+        }
+        else {
+            filter = filters.get('Type').find(o => o.name === 'Video');
+            ytsr.getFilters(filter.ref, function (err, filters) {
+                if (err) {
+                    console.log("htp2");
+                    try {
+                        throw err;
+                    }
+                    catch (err) {
+                        console.log(err)
+                        vkmusicsearch(message, music);
+                    }
+                }
+                else {
+                    var options = {
+                        limit: 1,
+                        nextpageRef: filter.ref,
+                    }
+                    ytsr(null, options, function (err, searchResults) {
+                        if (err) {
+                            console.log("htp3");
+                            try {
+                                throw err;
+                            }
+                            catch (err) {
+                                console.log(err)
+                                vkmusicsearch(message, music);
+                            }
+                        }
+                        else {
+                            console.log("результат мазафака");
+                            console.log(searchResults.items[0].link);
+                            vkplay(message, searchResults.items[0].link);
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
+
 
 client.login(token);
